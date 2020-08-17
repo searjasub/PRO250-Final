791eb628bee7ded2e039a056d93aa05338abb877
Searjasub Lopez <searjasub.lopez@outlook.com>
2020-08-17 13:24:57 -0600

    first start

---
+++ app/src/main/java/pro250/mobiledungeon/MainActivity.java
@@ -0,0 +1,16 @@
+package pro250.mobiledungeon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
