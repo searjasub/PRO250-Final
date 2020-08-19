3c91e039a3492d1fdc3cd4f6e71ceadde8372a62
CeceStoner <cstoner@student.neumont.edu>
2020-08-19 15:39:24 -0400

    Small Achievement Changes

--- app/src/main/java/pro250/mobiledungeon/java/achievements/AchievementStoreFactory.java
+++ app/src/main/java/pro250/mobiledungeon/java/achievements/AchievementStoreFactory.java
@@ -14,6 +14,7 @@ import com.eclipsesource.json.JsonObject;
 import com.eclipsesource.json.JsonObject.Member;
 import com.eclipsesource.json.JsonValue;
 
+import java.lang.reflect.Member;
 import java.util.Collection;
 
 /**
