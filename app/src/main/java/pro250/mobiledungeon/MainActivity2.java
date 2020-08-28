b198b3b207c531f501845490fd59308c2bc047c2
tikalaka <baydadan001@gmail.com>
2020-08-28 14:48:51 -0600

    Update MainActivity2.java

--- app/src/main/java/pro250/mobiledungeon/MainActivity2.java
+++ app/src/main/java/pro250/mobiledungeon/MainActivity2.java
@@ -17,12 +17,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import pro250.mobiledungeon.java.game.Game;
+import pro250.mobiledungeon.java.game.GameState;
+
 public class MainActivity2 extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = findViewById(R.id.toolbar);
